5e392a7fe25ab19305671a0f585b80872dca65e1
snehalsankhe <snehal.sankhe@raweng.com>
2022-03-09 14:22:40 +0530

    added file

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const fs = require('fs');
-const read = fs.createReadStream('./01-objects/file1.txt');
-const write = fs.createWriteStream('./01-objects/file2.txt');   
-read.pipe(write);
+const fs = require("fs");
 
+const asyncFunction = (err, data) => {
+    if (err) throw err;
+    // Display the file content
+    console.log("Data..........", data);
+    fs.writeFile('./write.txt', data, (err) => {
+        if (err) {
+            fs.writeFile('./error.txt', 'Error', (err) => {
+                if (err)  throw err;
+                console.log("Error........", err);
+            })
+        } else {
+            fs.writeFile('./error.txt', 'successful.', (err) => {
+                if (err)  throw err;
+                console.log("Message on error txt file...........", 'successful.');
+            })
+        }
+        console.log("Write Data ...............", data);
+    });
+}
+
+fs.readFile('./read.txt', "utf8", asyncFunction);
